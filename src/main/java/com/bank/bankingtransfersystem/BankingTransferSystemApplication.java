e398e0b9b54ac38c75119fc52ed66e7712ee1c74
yoonkyung <yoonkyung.oh@datarize.ai>
2026-08-07 15:21:24 +0900

    first commit

---
+++ src/main/java/com/bank/bankingtransfersystem/BankingTransferSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bank.bankingtransfersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingTransferSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingTransferSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bank/bankingtransfersystem/BankingTransferSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.bankingtransfersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingTransferSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
